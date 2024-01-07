c114f001da507c07544ba03556f1a60f11bb3991
Martin Kukolos <martin@kukolos.com>
2024-01-07 11:58:52 +0100

    update

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ try {
     const labels = github.context.payload.issue.labels.map(label => label.name);
     const title = github.context.payload.issue.title;
     const body = github.context.payload.issue.body;
+    const tags = labels.filter(label => label.startsWith('tag:')).map(label => label.replace('tag:', ''));
+    const type = labels.filter(label => label.startsWith('type:')).map(label => label.replace('type:', ''))[0];
+    const slug = title.toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g, '');
+
 
     console.log(`Labels: ${labels}`);
     console.log(`Title: ${title}`);
     console.log(`Body: ${body}`);
+    console.log(`Tags: ${tags}`);
+    console.log(`Type: ${type}`);
+    console.log(`Slug: ${slug}`);
 
 } catch (error) {
     core.setFailed(error.message);
